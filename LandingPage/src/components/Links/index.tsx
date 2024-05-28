662e5d18264c22b2adf54d574f9ba11d42a49f1b
Luiz Carlos <lcarlos.mln@gmail.com>
2024-05-28 07:53:54 -0300

    novos componentes

---
+++ LandingPage/src/components/Links/index.tsx
@@ -0,0 +1,16 @@
+function Links() {
+    return (
+        <div className="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
+            <ul className="navbar-nav mb-2 mb-lg-0 flex-grow-1 justify-content-center">
+                <li className="nav-item"><a href="#" className="nav-link aIndex me-5" id="#">Home</a></li>
+                <li className="nav-item"><a href="#" className="nav-link aIndex me-5" id="#">Sobre Nós</a></li>
+                <li className="nav-item"><a href="#" className="nav-link aIndex me-5" id="#">Contato</a></li>
+            </ul>
+            <form className="d-flex align-self-end" role="search">
+                <button className="btn btn-success align-self-end m-3 mgr2" id="btnind">WhatsApp</button>
+            </form>
+        </div>
+    )
+}
+
+export default Links;
---
+++ LandingPage/src/components/Logo/index.tsx
@@ -0,0 +1,9 @@
+function Logo() {
+    return (
+        <a className="navbar-brand img-fluid" href="#">
+            <img src="src/imgs/logo01.png" alt="Logotipo" id="imglogo" className="m-0 m-md-3 ms-md-5" />
+        </a>
+    )
+}
+
+export default Logo;
