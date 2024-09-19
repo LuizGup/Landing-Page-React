1fc208a4f4927a07c8a45bacc38a4d17617c1651
Luiz Carlos Filho <lcarlos.mln@gmail.com>
2024-09-19 11:54:57 -0300

    changes in paths

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -2,7 +2,7 @@ function Logo() {
   return (
     <a className="navbar-brand img-fluid" href="#">
       <img
-        src="src/imgs/logo01.png"
+        src="../../imgs/logo01.png"
         alt="Logotipo"
         id="imglogo"
         className="m-0 m-md-3 ms-md-5"
--- src/components/LogoAnimada/index.tsx
+++ src/components/LogoAnimada/index.tsx
@@ -3,12 +3,12 @@ function LogoAnimada() {
     <div className="col">
       <div className="position-relative text-center mt-3" id="containerImgs">
         <img
-          src="src/imgs/logonew5.png"
+          src="../../imgs/logonew5.png"
           alt=""
           className="img-fluid imgLogoIndex"
         />
         <img
-          src="src/imgs/LogoCodeCrafter2.png"
+          src="../../imgs/LogoCodeCrafter2.png"
           alt=""
           className="position-absolute top-50 start-50 translate-middle imgLogoIndex2"
         />
