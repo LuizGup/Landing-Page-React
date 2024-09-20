b497f3991cee1309b4f950980f5987d154e65f30
Luiz Carlos Filho <lcarlos.mln@gmail.com>
2024-09-20 11:09:31 -0300

    Update index.tsx

--- src/components/Planos/index.tsx
+++ src/components/Planos/index.tsx
@@ -7,33 +7,33 @@ function Planos() {
       <div className="container pt-5">
         <div className="row justify-content-around">
           <div className="col-md-auto text-center border border-2 border-info rounded p-5 bordersec1">
-            <img
+            {/* <img
               src="/imgs/icons8-html-5-48.png"
               alt="#"
               className="imgsec1 mb-1"
-            />
+            /> */}
             <p>
               <strong>HTML5</strong>
             </p>
             <p>20 horas - prática/teoria</p>
           </div>
           <div className="col-md-auto text-center border border-2 border-info rounded p-5 bordersec1">
-            <img
+            {/* <img
               src="/imgs/icons8-css3-48.png"
               alt="#"
               className="imgsec1 mb-1"
-            />
+            /> */}
             <p>
               <strong>CSS3</strong>
             </p>
             <p>18 horas - prática/teoria</p>
           </div>
           <div className="col-md-auto text-center border border-2 border-info rounded p-5 bordersec1">
-            <img
+            {/* <img
               src="/imgs/icons8-javascript-48.png"
               alt="#"
               className="imgsec1"
-            />
+            /> */}
             <p>
               <strong>JAVASCRIPT</strong>
             </p>
